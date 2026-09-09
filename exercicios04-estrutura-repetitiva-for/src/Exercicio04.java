5881abd20e7d76a51bf26f191e228dd8f8d0d823
Jair de Sousa <jairsmarques1126@gmail.com>
2026-09-09 16:09:29 -0300

    resolvendo os exercícios da aula sobre estrutura repetitiva for

---
+++ exercicios04-estrutura-repetitiva-for/src/Exercicio04.java
@@ -0,0 +1,35 @@
+/*
+ 	Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão
+ 	do primeiro pelo segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao 
+ 	impossivel".
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		int x, y;
+		for (int i = 0; i < n; i++) {
+			x = sc.nextInt();
+			y = sc.nextInt();
+
+			if (y != 0) {
+				System.out.println((double) x / y);
+			} else {
+				System.out.println("divisão impossível");
+			}
+		}
+
+		sc.close();
+
+	}
+
+}
